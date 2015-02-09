ef9a34f09b3a1c9381c551c0518b7f66524546b4
nithin mohan <nithinmohan94@gmail.com>
2015-02-08 19:19:59 -0800

    rebasing with master

--- index2.php
+++ index2.php
@@ -8,6 +8,8 @@
 		<script type="text/javascript" src="js/tweenmax.js"></script>
 		<script type="text/javascript" src="js/scrollmagic.js"></script>
 		<script type="text/javascript" src="js/scrollmagic.debug.js"></script>
+		<script type="text/javascript" src="js/ScrollToPlugin.js"></script>
+		<script type="text/javascript" src="js/cssplugin.js"></script>
 		<link rel="stylesheet" type="text/css" href="css/index2.css"/>
 	</head>
 	<body>
@@ -28,9 +30,38 @@
 	</body>
 	<script>
 	$(document).ready(function($) {
-		// init controller
-		var controller = new ScrollMagic();
+	  var $window = $(window);
+        var scrollTime = 1;
+        var scrollDistance = 250;
+        var iPadAgent = navigator.userAgent.match(/iPad/i) != null;
+        var iPodAgent = navigator.userAgent.match(/iPhone/i) != null;
+        var AndroidAgent = navigator.userAgent.match(/Android/i) != null;
+        var webOSAgent = navigator.userAgent.match(/webOS/i) != null;
 
+        
+        if(iPadAgent || iPodAgent || AndroidAgent || webOSAgent){
+            var controller = new ScrollMagic({container: "#scroll_wrapper"});
+            $('#scroll_wrapper').height($(window).height());
+            $('#scroll_wrapper').width($(window).width()); 
+        }
+        else{
+			$window.on("mousewheel DOMMouseScroll", function(event){
+	                
+	                event.preventDefault();	
+	                          
+	                var delta = event.originalEvent.wheelDelta/120 || -event.originalEvent.detail/3;
+	                var scrollTop = $window.scrollTop();
+	                var finalScroll = scrollTop - parseInt(delta*scrollDistance);
+	                
+	                TweenMax.to($window, scrollTime, {
+	                    scrollTo:{ y: finalScroll, autoKill:false },
+	                        ease: Power1.easeOut,
+	                        overwrite: 5							
+	                    });
+	            });
+			var controller = new ScrollMagic();
+		// 	// init controller
+		}
 		// build scenes
 		new ScrollScene({triggerElement: "#sec1"})
 						.setClassToggle("#grad1", "active") // add class toggle
