ac9ba348ed3655f331a3fb0eaa032ee50fd05618
tarunuday <tarunuday@gmail.com>
2015-02-26 13:57:11 +0530

    sdfsd

--- main.php
+++ main.php
@@ -121,6 +121,58 @@
 <!--**********************************************4INFO START*******************-->				
 			<div id="Info" class="sec">
 				<div style="height:100%;width:70%;float:left">
+					<div style="height:100%;width:70%;float:left">
+						<h2>	REGISTRATION RULES</h2>
+						</br><p>
+							1. Participants should have a confirmed Ragam ID for participating in any event.
+							</br>
+							2. Online provided Ragam ID becomes valid only when it is confirmed at the 
+							Registration Desk.
+							</br>
+							3. Participants should bring a valid college ID as the proof for identity. Unless you’re 
+							Batman.  Everyone knows Batman.
+							</br>
+							4. On the spot registration for Ragam ID as well as events will be provided at the 
+							registration desk.
+							</br>
+							5. For team events, every member of the team should possess Ragam ID.
+							</br>
+							6. Participants who have registered for workshops will not be allowed to participate in 
+							events unless they do event registration and vice-versa.
+							</br>
+							7. Once registered, participant can take part in any event or free workshop of Ragam.
+							</br>
+							8. Only those with Ragam ID will be given accommodation.
+							</br>
+							9. Prizes and certificates can be collected from the Registration Desk, once the 
+							results are out. For team events, prizes will be handed over to the EVENT TEAM 
+							CAPTAIN. 
+							</br>
+							10. There will no cash prize settlement after 17th March.
+							Registration fees: Rs.150 Hospitality fees: Rs.50 (in addition a caution deposit of 
+							Rs.50 will also be collected, which will be refunded at the return of utilities 
+							provided) Workshops: As per the stipulated fee (No other registration fee required) 
+							For the special attention of the participants coming in groups :
+							</br>
+							11. A member of the group should be assigned as the group leader and he/she must 
+							have a list of all group members along with their RAGAM ID's and their college IDs 
+							at the time of check in. Carrying such a list can considerably reduce the delay at 
+							the registration desk. We like to move it, move it too.
+							</br>
+							12. A representative from each group is strongly advised to contact the Hospitality 
+							Manager prior to their arrival at the campus to avoid delays in registration, for 
+							confirmation of accommodation and to avail transportation facilities during odd 
+							hours from the Calicut city. 
+							</br>
+							<b>NOTE:</b> 1. The Ragam Registration Committee reserves the right to deduct from the 
+							caution deposit for any loss/damage caused by the participants.  2. The Ragam 
+							Registration Committee will have the sole right to disqualify any team engaged in 
+							any form of malpractice during Registration. Remember: with great power, comes 
+							great responsibility. 3. Ragam'14 Registration Desk will be open from 6.00 PM, 
+							13th March to 9.00AM, 17th March. In case of early arrival at NIT Campus, contact 
+							the hospitality manager to avail temporary accommodation.
+						</p>
+					</div>
 				</div>
 				<div style="height:100%;width:30%;float:left">
 					<div id="info-head-place" class="head-place">
@@ -129,7 +181,6 @@
 						</div>
 					</div>
 					<div id="info-words-place" class="words-place">
-						ENTER TEXT FOR INFO
 					</div>
 					<div id="info-sprite" class="sprite">
 					<iframe src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d7824.467159110062!2d75.93262136303221!3d11.317630530845543!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sin!4v1424820583878" width="100%" height="100%" frameborder="0" style="border:0"></iframe>
